33fc482a36034ba06a65f742e45ba6d4c77bc56a
vekt0R-HUB <d0t42f4n@gmail.com>
2023-04-12 22:10:34 -0400

    Progress with some op combinations

--- main.c
+++ main.c
@@ -59,7 +59,6 @@ void signalHandler_child(int p){
      * block if a child was cleaned up in another part of the program. */
     while (waitpid(-1, NULL, WNOHANG) > 0) {
     }
-    printf("\n");
 }
 
 void signalHandler_int(int p){
@@ -119,59 +118,64 @@ int ntl_exit(char **args) {
     return 0;
 }
 
-#define READ_END 0
-#define WRITE_END 1
+void RedirectOutput(char* outputFile, int fileDescriptor){
+    fileDescriptor = open(outputFile, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+    dup2(fileDescriptor, STDOUT_FILENO);
+    close(fileDescriptor);
+}
 
-int ntl_pipe(char **args, char **pipedArgs){
-    int pipefd[2];
+void RedirectInput(char* inputFile, int fileDescriptor){
+    fileDescriptor = open(inputFile, O_RDONLY, 0600);
+    dup2(fileDescriptor, STDIN_FILENO);
+    close(fileDescriptor);
+}
 
-    // Create pipe
-    if (pipe(pipefd) == -1) {
-        perror("Failed to create pipe");
-        exit(EXIT_FAILURE);
+void RedirectAppendOutput(char* outputFile, int fileDescriptor){
+    fileDescriptor = open(outputFile, O_CREAT | O_APPEND | O_WRONLY, 0600);
+    dup2(fileDescriptor, STDOUT_FILENO);
+    close(fileDescriptor);
+}
+
+void RedirectPipeOutput(int* pipeFd){
+    dup2(pipeFd[1], STDOUT_FILENO);
+    close(pipeFd[0]);
+    close(pipeFd[1]);
+    no_reprint_prmpt = 1;
+}
+
+int HandlePipeAndPipeOutput(int* pipeFd, char** pipeArgs, int option, char* outputFile, int fileDescriptor){
+    int err = -1;
+    pid_t pid2;
+
+    // execute the piped command
+    if((pid2=fork())==-1){
+        printf("Child process could not be created\n");
+        return -1;
     }
 
-    // Fork first child
-    pid = fork();
-    if (pid == -1) {
-        perror("Failed to fork first child");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        // First child process: redirect stdout to pipe write end
-        close(pipefd[READ_END]);
-        dup2(pipefd[WRITE_END], STDOUT_FILENO);
-        close(pipefd[WRITE_END]);
+    if (pid2 == 0) {
+        // redirect input from pipe
+        dup2(pipeFd[0], STDIN_FILENO);
+        close(pipeFd[0]);
+        close(pipeFd[1]);
 
-        // Execute first command
-        if (execvp(args[0], args) == -1) {
-            perror("Failed to execute first command");
-            exit(EXIT_FAILURE);
+        if(option == 1) {
+            RedirectOutput(outputFile, fileDescriptor);
+//            printf("Got to option 1");
+        }
+
+        if (execvp(pipeArgs[0], pipeArgs) == -1){
+            printf("err");
+            kill(getpid(),SIGTERM);
+            return -1;
         }
     }
 
-    // Fork second child
-    pid = fork();
-    if (pid == -1) {
-        perror("Failed to fork second child");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        // Second child process: redirect stdin to pipe read end
-        close(pipefd[WRITE_END]);
-        dup2(pipefd[READ_END], STDIN_FILENO);
-        close(pipefd[READ_END]);
+    no_reprint_prmpt = 0;
 
-        // Execute second command
-        if (execvp(pipedArgs[0], pipedArgs) == -1) {
-            perror("Failed to execute second command");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    // Parent process: wait for both children to finish
-    close(pipefd[READ_END]);
-    close(pipefd[WRITE_END]);
-    wait(NULL);
-    wait(NULL);
+    close(pipeFd[0]);
+    close(pipeFd[1]);
+    waitpid(pid2,NULL,0);
 
     return 1;
 }
@@ -180,7 +184,6 @@ int ntl_launch(char **args, char* inputFile, char* outputFile, char **pipeArgs, 
     int err = -1;
     int fileDescriptor;
     int pipeFd[2];
-    pid_t pid2;
 
     if (option == 4 && pipe(pipeFd) < 0) {
         perror("Pipe error");
@@ -194,59 +197,48 @@ int ntl_launch(char **args, char* inputFile, char* outputFile, char **pipeArgs, 
 
     if(pid==0){
         if (option == 1){
-            fileDescriptor = open(outputFile, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-            dup2(fileDescriptor, STDOUT_FILENO);
-            close(fileDescriptor);
+            RedirectOutput(outputFile, fileDescriptor);
         }
         else if (option == 2){
-            fileDescriptor = open(inputFile, O_RDONLY, 0600);
-            dup2(fileDescriptor, STDIN_FILENO);
-            close(fileDescriptor);
+            RedirectInput(inputFile, fileDescriptor);
         }
         else if(option == 3){
-            fileDescriptor = open(outputFile, O_CREAT | O_APPEND | O_WRONLY, 0600);
-            dup2(fileDescriptor, STDOUT_FILENO);
-            close(fileDescriptor);
+            RedirectAppendOutput(outputFile, fileDescriptor);
         }
-
-        if (option == 4) {
-            // redirect output to pipe
-            dup2(pipeFd[1], STDOUT_FILENO);
-            close(pipeFd[0]);
-            close(pipeFd[1]);
+        else if (option == 4) {
+            RedirectPipeOutput(pipeFd);
+        }
+        else if (option == 5){
+            RedirectInput(inputFile, fileDescriptor);
+            RedirectOutput(outputFile, fileDescriptor);
+        }
+        else if (option == 6){
+            //TODO: doesn't work
+            RedirectInput(inputFile, fileDescriptor);
+            RedirectPipeOutput(pipeFd);
+            option = 4;
+        }
+        else if (option == 7){
+            RedirectInput(inputFile, fileDescriptor);
+            RedirectAppendOutput(outputFile, fileDescriptor);
         }
 
         if (execvp(args[0],args)==err){
             printf("err");
             kill(getpid(),SIGTERM);
-            return 1;
+            return -1;
         }
     }
 
     if (option == 4) {
-        // execute the piped command
-        if((pid2=fork())==-1){
-            printf("Child process could not be created\n");
-            return -1;
-        }
-
-        if (pid2 == 0) {
-            // redirect input from pipe
-            dup2(pipeFd[0], STDIN_FILENO);
-            close(pipeFd[0]);
-            close(pipeFd[1]);
-
-            if (execvp(pipeArgs[0], pipeArgs) == err){
-                printf("err");
-                kill(getpid(),SIGTERM);
-                return 1;
-            }
-        }
-
-        close(pipeFd[0]);
-        close(pipeFd[1]);
-        waitpid(pid2,NULL,0);
+        printf("Option 4");
+        HandlePipeAndPipeOutput(pipeFd, pipeArgs, 0, NULL, fileDescriptor);
     }
+    if (option == 8){
+        //TODO: doesn't work
+        HandlePipeAndPipeOutput(pipeFd, pipeArgs, 1, outputFile, fileDescriptor);
+    }
+
 
     waitpid(pid,NULL,0);
 
@@ -276,25 +268,32 @@ int ntl_parsing(char **commands, char **separators, int numCommands, int numSepa
                     //Option 3 for >>
                     ntl_launch(commands + start, NULL, commands[i], NULL, 3);
                 }
+
                 currSeparator++;
             }
             else if(separators[currSeparator] != NULL && strcmp(separators[currSeparator], "<") == 0){
+                while (commands[i++] != NULL);
+                if(commands[i] == NULL){
+                    printf("Not enough arguments");
+                    return 1;
+                }
+
                 if(separators[currSeparator+1] != NULL && strcmp(separators[currSeparator+1], ">") == 0){
-                    printf("WIP!");
+                    ntl_launch(commands + start, commands[i], commands[i+2], NULL, 5);
+                    i+=2;
+                    currSeparator++;
                 }
                 else if(separators[currSeparator+1] != NULL && strcmp(separators[currSeparator + 1], "|") == 0){
-                    printf("WIP!");
+                    ntl_launch(commands + start, commands[i], NULL, commands + i + 2, 6);
+                    i+=2;
+                    currSeparator++;
                 }
                 else if(separators[currSeparator+1] != NULL && strcmp(separators[currSeparator + 1], ">>") == 0){
-                    printf("WIP!");
+                    ntl_launch(commands + start, commands[i], commands[i+2], NULL, 7);
+                    i+=2;
+                    currSeparator++;
                 }
                 else{
-                    while (commands[i++] != NULL);
-                    if(commands[i] == NULL){
-                        printf("Not enough arguments");
-                        return 1;
-                    }
-
                     // Option 2 for <
                     ntl_launch(commands + start, commands[i], NULL, NULL, 2);
                 }
@@ -308,7 +307,20 @@ int ntl_parsing(char **commands, char **separators, int numCommands, int numSepa
                     return 1;
                 }
 
-                ntl_launch(commands + start, NULL, NULL, commands + i, 4);
+                if(separators[currSeparator+1] != NULL && strcmp(separators[currSeparator+1], ">") == 0){
+                    int startPiped = i;
+                    while (commands[i++] != NULL);
+                    if(commands[i] == NULL){
+                        printf("Not enough arguments");
+                        return 1;
+                    }
+
+                    ntl_launch(commands + start, NULL, commands[i], commands + startPiped, 8);
+                    currSeparator++;
+                }
+                else{
+                    ntl_launch(commands + start, NULL, NULL, commands + i, 4);
+                }
             }
             else{
                 ntl_launch(commands + start, NULL, NULL, NULL, 0);
@@ -365,7 +377,7 @@ void ntl_loop() {
 
 
     do{
-        printf("ntl> ");
+        if(no_reprint_prmpt == 0) printf("ntl> ");
 
         memset ( line, '\0', MAXLINE );
 
